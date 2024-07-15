dda1e50ffed185ae626a21e5c0ad51cbef408b7c
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2024-07-15 14:29:37 +0500

    commit

---
+++ src/constants.js
@@ -0,0 +1,54 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Web development",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "UI/UX design",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Aisha Jaweria';
+
+export const experiences = [
+    {
+        'company': 'InternnCraft',
+        'role': 'full Stack Developer',
+        'duration': 'April 2024 - Present',
+        'logo': 'https://internncraft.com/wp-content/uploads/2024/03/Untitled_design__1_-removebg-preview-114x114.png',
+        'points': [
+            'Built and enhanced dynamic web applications using React.js for front-end development and Node.js/Express for back-end services, ensuring responsive and interactive user experiences.',
+            'Designed and implemented database schemas using MongoDB and SQL databases, optimizing data storage and retrieval processes to improve application performance.',
+            'Created and integrated RESTful APIs for seamless communication between front-end and back-end systems, enabling efficient data exchange and functionality.📈.',
+            'Conducted performance tuning and optimization of both client-side and server-side components, leading to improved application speed and user satisfaction.⚙️.'
+        ],
+        'url': 'https://internncraft.com/',
+    },
+    {
+        'company': 'CodSoft',
+        'role': 'UI/UX DESIGNER',
+        'duration': 'Sep 2023 - Oct 2023',
+        'logo': 'https://assets.zyrosite.com/cdn-cgi/image/format=auto,w=608,fit=crop,q=95/Aq20eV79zLfpXV6b/logo-png-mnl7npnlXjHPl9KV.png',
+        'points': [
+            'As a UI/UX designer, where I designed a visually appealing and user-friendly login webpage, Bloging Website. enhancing the user experience and accessibility.',
+            'Enhancing the user experience and accessibility📱.'
+        ],
+        'url': 'https://www.codsoft.in/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = 'service_2l8b25h';
+export const EMAIL_JS_TEMPLATE_ID = 'template_rdp18jn';
+export const EMAIL_JS_PUBLIC_KEY = 'EjvrOkmNMonmMwZIJ';
