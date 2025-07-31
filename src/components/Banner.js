3020c30681a1c3253ec96f498ef453b339b57fd8
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2025-07-31 16:35:55 +0500

    change banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,9 @@
-import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
+import { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
 import TrackVisibility from 'react-on-screen';
+import headerImg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
@@ -56,10 +56,10 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>{`Hi! I'm Aisha Jaweria, a`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Passionate Full Stack Web Developer
-                    Building innovative and scalable web solutions to bring your ideas to life.
-                    Combining front-end finesse with back-end power to build dynamic web experiences.</p>
+                  <h1>{`Hi! I'm Aisha Jaweria, a`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "React Native", "Full Stack Developer"  ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Passionate React native Developer
+                    Building innovative and scalable web and Mobile solutions to bring your ideas to life.
+                    Combining front-end finesse with back-end power to build dynamic web and Mobile experiences.</p>
                   <button onClick={() => window.location.href = 'https://drive.google.com/uc?export=download&id=1hIXDia7lTlJnIj_Zqpt6AnutFjHk8JzL'} className="p-3 bg-blue-500 text-white rounded-lg flex items-center">
                     Let’s Connect <ArrowRightCircle size={25} className="ml-2" />
                   </button>
