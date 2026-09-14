3541837344dd0b83592325004f39244c8bc2c813
aishajaweria <82979914+aishajaweria@users.noreply.github.com>
2026-09-14 15:35:50 +0300

    added name

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,10 +56,12 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                  <h1>{`Hi! I'm Aisha Jaweria, a`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "React Native", "Full Stack Developer"  ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Passionate React native Developer
-                    Building innovative and scalable web and Mobile solutions to bring your ideas to life.
-                    Combining front-end finesse with back-end power to build dynamic web and Mobile experiences.</p>
+                  <h1>{`Hi! I'm Aisha Jaweria, a`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ 
+                  "Backend Developer","React Native", "Full Stack Developer"  ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Passionate Backend Developer
+                    Backend-focused Software Developer with hands-on experience in SQL query optimization, database and product-catalog
+management, and Python-based process automation, gained alongside full-stack and mobile app development using React, React
+Native, and Firebase.</p>
                   <button onClick={() => window.location.href = 'https://drive.google.com/uc?export=download&id=1hIXDia7lTlJnIj_Zqpt6AnutFjHk8JzL'} className="p-3 bg-blue-500 text-white rounded-lg flex items-center">
                     Let’s Connect <ArrowRightCircle size={25} className="ml-2" />
                   </button>
